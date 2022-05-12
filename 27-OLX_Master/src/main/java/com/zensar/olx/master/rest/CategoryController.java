c7c384eb79963bb000bb11e8872b4653894f8297
RachanaS27 <r.sridhar@zensar.com>
2022-05-12 09:10:15 +0530

    connecting with angular

--- 27-OLX_Master/src/main/java/com/zensar/olx/master/rest/CategoryController.java
+++ 27-OLX_Master/src/main/java/com/zensar/olx/master/rest/CategoryController.java
@@ -3,6 +3,7 @@ package com.zensar.olx.master.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,8 @@ import com.zensar.olx.master.bean.Category;
 import com.zensar.olx.master.service.CategoryService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
+
 public class CategoryController {
 
 	@Autowired
